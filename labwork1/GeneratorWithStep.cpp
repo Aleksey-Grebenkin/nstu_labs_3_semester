de0975030f302e32da25e148a88ab26680e03524
Aleksey-Grebenkin <61308932+Aleksey-Grebenkin@users.noreply.github.com>
2020-09-13 00:09:23 +0700

    Update GeneratorWithStep.cpp

--- labwork1/GeneratorWithStep.cpp
+++ labwork1/GeneratorWithStep.cpp
@@ -8,7 +8,7 @@ using namespace std;
 GeneratorWithStep::GeneratorWithStep(const string& name,double first_number, double step, int N)
 {
     if (N <= 0)
-        throw invalid_argument("ÌÂ‰ÓÔÛÒÚËÏÓÂ ÁÌ‡˜ÂÌËÂ N");
+        throw invalid_argument("invalid variable value");
     sequence.push_back(first_number);
     this->name = name;
     this->step = step;
@@ -49,7 +49,7 @@ string GeneratorWithStep::getName() const
 double GeneratorWithStep::AverageNumbers() const
 {
     if (N > sequence.size())
-        throw out_of_range("‚˚ıÓ‰ Á‡ ÔÂ‰ÂÎ˚ ÁÌ‡˜ÂÌËÈ");
+        throw out_of_range("invalid variable value");
     double sum = 0.0;
 
     int l = sequence.size();
